347f99032a89b6d73899659be091bf9da4dbffd6
Kent Aguilar <kentaguilar@gmail.com>
2020-07-15 00:29:13 +0800

    initial commit

---
+++ CodeFirstEF/Core/AppContextMain.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using CodeFirstEF.Models;
+
+namespace CodeFirstEF.Core
+{
+    public class AppContextMain : DbContext
+    {
+        public virtual DbSet<Student> Students { get; set; }        
+
+        public AppContextMain()
+            : base("name=MainConnectionString")
+        {
+            this.Configuration.LazyLoadingEnabled = false;
+            this.Configuration.ProxyCreationEnabled = false;
+        }
+    }
+}
---
+++ CodeFirstEF/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CodeFirstEF.Core;
+using CodeFirstEF.Models;
+using System.Configuration;
+
+namespace CodeFirstEF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(string.Format("Starting {0}", ConfigurationManager.AppSettings["AppName"]));
+
+
+            using (var appContextMain = new AppContextMain())
+            {
+                appContextMain.Students.Add(new Student
+                {
+                    Id = 2,
+                    Name = "Angel Locsin",
+                    Status = "Active"
+                });
+
+                appContextMain.SaveChanges();
+            }
+
+
+            Console.WriteLine("Completed");
+            Console.ReadKey();
+        }
+    }
+}
